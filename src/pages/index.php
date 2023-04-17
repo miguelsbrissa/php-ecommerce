1accd4515a6b18d14066cafc2beb2755deda51d6
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-04-17 13:32:51 -0300

    feat: :sparkles: Add homepage with style

---
+++ src/pages/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="https://kit.fontawesome.com/f673a817b4.js" crossorigin="anonymous"></script>
+    <title>E-commerce with PHP</title>
+</head>
+
+<body>
+    <nav class="menu-nav">
+        <ul>
+            <a href="http://localhost/php-ecommerce/src/pages/index.php" class="menu-nav-item logo">
+                <i class="fa-solid fa-seedling"></i>
+                HealthyFood
+            </a>
+        </ul>
+        <div class="menu-nav center">
+            <ul>
+                <a href="http://localhost/php-ecommerce/src/pages/produtos.php" class="menu-nav-item">Produtos <i class="menu-nav-item drop"></i></a>
+
+            </ul>
+            <ul>
+                <a href="http://localhost/php-ecommerce/src/pages/sobre.php" class="menu-nav-item">Sobre</a>
+            </ul>
+            <ul>
+                <a href="http://localhost/php-ecommerce/src/pages/fale.php" class="menu-nav-item">Fale Conosco</a>
+            </ul>
+        </div>
+        <ul>
+            <a href="http://localhost/php-ecommerce/src/pages/index.php" class="menu-nav-item bag"><i class="fa-solid fa-bag-shopping"></i></a>
+        </ul>
+    </nav>
+    <div class="content">
+        <h1 class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+
+        <div class="cards">
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img"/>
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price"/>
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img"/>
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price"/>
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img"/>
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price"/>
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img"/>
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price"/>
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img"/>
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price"/>
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img"/>
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price"/>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
