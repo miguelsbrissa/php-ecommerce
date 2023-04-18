5563b7acdda0e68f391276d2bbba484e35b879ce
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-04-18 16:10:15 -0300

    feat: :sparkles: Cria pagina de Produtos e divide o CSS em diferentes arquivos

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/index.css">
+    <link rel="stylesheet" href="../style/nav.css">
+    <link rel="stylesheet" href="../style/footer.css">
     <script src="https://kit.fontawesome.com/f673a817b4.js" crossorigin="anonymous"></script>
     <title>E-commerce with PHP</title>
 </head>
@@ -14,7 +17,6 @@
     <?php include '../components/nav.php'; ?>
     <div class="content">
         <h1 class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-
         <div class="cards">
             <div class="cards item">
                 <img src="../images/apple.png" alt="Apple" class="img" />
