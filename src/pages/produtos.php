264a81d18b5113492a3861da6806305a635a19a9
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-04-18 17:07:29 -0300

    feat: :sparkles: Adiciona conteudo e estilo na pagina de Produtos

--- src/pages/produtos.php
+++ src/pages/produtos.php
@@ -16,9 +16,47 @@
 <body>
     <?php include '../components/nav.php'; ?>
     <div class="content">
-    
-    </div>
-    <?php include '../components/footer.php'; ?>
+        <div class="top-cards">
+            <h1 class="cat-name">Categorie name</h1>
+            <div class="input-icon">
+                <i class="fa-solid fa-magnifying-glass icon"></i>
+                <input type="text" name="search" id="search" class="search-input" placeholder="Pesquise um item...">
+            </div>
+        </div>
+        <div class="cards">
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img" />
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price" />
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img" />
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price" />
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img" />
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price" />
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img" />
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price" />
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img" />
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price" />
+            </div>
+            <div class="cards item">
+                <img src="../images/apple.png" alt="Apple" class="img" />
+                <h1 class="item-name">Apple</h1>
+                <input type="button" value="R$1,99" class="button-price" />
+            </div>
+        </div>
+
+        <?php include '../components/footer.php'; ?>
 </body>
 
 </html>
