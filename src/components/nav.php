a95611bfdb0e6df072099a4b7c3e16dee3a3545f
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-04-18 15:47:42 -0300

    feat: :lipstick: Add dropdown to Produtos option

--- src/components/nav.php
+++ src/components/nav.php
@@ -7,7 +7,16 @@
     </ul>
     <div class="menu-nav center">
         <ul>
-            <a href="http://localhost/php-ecommerce/src/pages/produtos.php" class="menu-nav-item">Produtos <i class="fa-solid fa-chevron-down"></i></i></a>
+            <div class="dropdown">
+                <a href="http://localhost/php-ecommerce/src/pages/produtos.php" class="menu-nav-item dropdownbtn">Produtos <i class="fa-solid fa-chevron-down"></i></i></a>
+                <div class="dropdown-content">
+                    <a href="#" class="dropdown-item">Frutas</a>
+                    <a href="#" class="dropdown-item">Snacks</a>
+                    <a href="#" class="dropdown-item">Café</a>
+                    <a href="#" class="dropdown-item">Vegetais</a>
+                </div>
+            </div>
+
         </ul>
         <ul>
             <a href="http://localhost/php-ecommerce/src/pages/sobre.php" class="menu-nav-item">Sobre</a>
