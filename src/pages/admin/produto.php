cda035ea000fa720832fa70d48e07fa0c74e9ed5
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-05-09 15:54:12 -0300

    feat: :sparkles: Cria páginas de admin

---
+++ src/pages/admin/produto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/index.css">
+    <link rel="stylesheet" href="../style/nav.css">
+    <link rel="stylesheet" href="../style/footer.css">
+    <script src="https://kit.fontawesome.com/f673a817b4.js" crossorigin="anonymous"></script>
+    <title>E-commerce with PHP - Admin</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
