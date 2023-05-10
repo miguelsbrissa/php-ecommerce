b8fc2683e54933297a0db4f47d956bcc5569d3db
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-05-10 16:06:27 -0300

    feat: :sparkles: Torna funcional barra de busca

--- src/pages/produtos.php
+++ src/pages/produtos.php
@@ -17,9 +17,13 @@
     <?php include '../components/nav.php'; ?>
     <?php include '../controller/produtoController.php'; ?>
     <?php
+    $produtosByName = '';
     if (isset($_GET['categoria'])) {
         $catName =  $_GET['categoria'];
         $produtosByCat = findProdutoByCategoriaDB($catName, $conn);
+    }
+    if (isset($_POST['searchButton'])) {
+        $produtosByName = findAllProdutosLikeNameController($_POST['searchField'], $conn);
     }
     ?>
     <div class="content">
@@ -30,21 +34,32 @@
                 <h1 class="erro"> A categoria não foi selecionada! </h1>
             <?php endif; ?>
             <form class="search-field" method="post">
-                <input type="text" name="searchField" id="search" class="search-input" placeholder="Pesquise um item..." >
+                <input type="text" name="searchField" id="search" class="search-input" placeholder="Pesquise um item...">
                 <button type="submit" name="searchButton" class="search-button"><i class="fa-solid fa-magnifying-glass icon"></i></button>
             </form>
         </div>
         <div class="cards">
             <?php if (empty($produtosByCat)) : ?>
                 <h1 class="erro">Não existem produtos nessa categoria ainda! </h1>
+            <?php elseif (!empty($produtosByCat) && !is_array($produtosByName)) : ?>
+                <?php foreach ($produtosByCat as $produto) : ?>
+                    <div class="cards item">
+                        <img src="../images/<?php echo $produto['img'] ?>" alt="Apple" class="img" />
+                        <h1 class="item-name"><?php echo $produto['nome'] ?></h1>
+                        <a href="http://localhost/php-ecommerce/src/pages/produto.php?produto=<?php echo $produto['nome'] ?>" class="button-price"><?php echo str_replace('.', ',', $produto['preco']); ?></a>
+                    </div>
+                <?php endforeach; ?>
+            <?php elseif (is_array($produtosByName)) : ?>
+                <?php foreach ($produtosByName as $produto) : ?>
+                    <div class="cards item">
+                        <img src="../images/<?php echo $produto['img'] ?>" alt="Apple" class="img" />
+                        <h1 class="item-name"><?php echo $produto['nome'] ?></h1>
+                        <a href="http://localhost/php-ecommerce/src/pages/produto.php?produto=<?php echo $produto['nome'] ?>" class="button-price"><?php echo str_replace('.', ',', $produto['preco']); ?></a>
+                    </div>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <h1 class="erro">Produto(s) não encontrado(s)</h1>
             <?php endif; ?>
-            <?php foreach ($produtosByCat as $produto) : ?>
-                <div class="cards item">
-                    <img src="../images/<?php echo $produto['img'] ?>" alt="Apple" class="img" />
-                    <h1 class="item-name"><?php echo $produto['nome'] ?></h1>
-                    <a href="http://localhost/php-ecommerce/src/pages/produto.php?produto=<?php echo $produto['nome']?>" class="button-price"><?php echo str_replace('.', ',', $produto['preco']); ?></a>
-                </div>
-            <?php endforeach; ?>
         </div>
         <?php include '../components/footer.php'; ?>
     </div>
